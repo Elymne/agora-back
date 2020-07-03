5f39a30281549e7a33a37f038f6e04c87a804094
sacha DJURDJEVIC <sacha.djurdjevic@gmail.com>
2020-07-04 00:24:00 +0200

    Structure initialization

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const app = require("./server")
+const sequelize = require("./configurations/db")
+const logger = require("./configurations/logger")
+const http = require("http")
+
+const port = process.env.PORT
+
+sequelize.sync().then(() => {
+    const server = http.createServer(app)
+
+    server.listen(port, () => {
+        logger.info(`App running on port ${process.env.PORT}.`)
+    })
+})
